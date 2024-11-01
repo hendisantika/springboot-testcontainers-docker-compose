2aecbad26d2579d86068e1bb90c60581f099f840
hendisantika <hendisantika@yahoo.co.id>
2024-11-01 09:18:35 +0700

    Add required libs🫸🌀✏️📗🐧🐳⬆

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,13 +24,23 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-rest")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("org.apache.commons:commons-csv:1.10.0")
+    implementation("commons-io:commons-io:2.15.1")
+
+    runtimeOnly("org.postgresql:postgresql:42.7.1")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.testcontainers:testcontainers:1.19.7")
+    testImplementation("org.testcontainers:junit-jupiter:1.19.7")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
