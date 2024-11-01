ca4d38235e8c2f8160532101df5a2bb6b1b51880
hendisantika <hendisantika@yahoo.co.id>
2024-11-01 09:19:54 +0700

    Add Person Domain🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/testcontainers/domain/Person.java
@@ -0,0 +1,44 @@
+package id.my.hendisantika.testcontainers.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : testcontainers
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/11/24
+ * Time: 09.19
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "person")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "age")
+    private Integer age;
+}
